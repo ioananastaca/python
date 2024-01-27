a87ca9a38874426b9bb3c8056bbc60f2610cdfa6
oana.n <onastaca@gmail.com>
2024-01-27 15:26:16 +0200

    first try with python

---
+++ begginer_projects/Demo/main.py
@@ -0,0 +1,32 @@
+#Mini projects
+
+#1. Create a greeting for your program.
+# print("Welcome to the Band Name Generator")
+# #2. Ask the user for the city that they grew up in.
+# city = input("What's name of the city you grew up in?\n")
+# #3. Ask the user for the name of a pet.
+# pet = input("What's name of a pet?\n")
+# #4. Combine the name of their city and pet and show them their band name.
+# print(city+pet)
+# #5. Make sure the input cursor shows on a new line:
+
+# ------------------------------
+#2. If the bill was $150.00, split between 5 people, with 12% tip.
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+# print("Welcome to the tip calculator.")
+# bill = float(input("What was teh total bill?\n"))
+# tip = int(input("What percentage tip would you like to give? 10,12, or 15?\n"))
+# pers = int(input("How many people to split the bill?\n"))
+# tip_as_percent = tip / 100
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / pers
+# final_amount = "{:2f}".format(bill_per_person)
+# print(f"Each person should pay: ${final_amount}")
+
+
