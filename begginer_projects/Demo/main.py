ec74c01b600adb42b261f470ae2909235713a456
oana.n <onastaca@gmail.com>
2024-01-29 17:48:24 +0200

    small projects

--- begginer_projects/Demo/main.py
+++ begginer_projects/Demo/main.py
@@ -29,4 +29,70 @@
 # final_amount = "{:2f}".format(bill_per_person)
 # print(f"Each person should pay: ${final_amount}")
 
+#ticket machine
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height?\n"))
+#
+# if height >= 120:
+#     print("You can ride. You can buy a ticket")
+#     age = int(input("What is your age?\n"))
+#     bill = 0
+#     if age < 12:
+#         print("Please pay $5")
+#         bill = 5
+#     elif age <= 18:
+#         print("Please pay $8")
+#         bill = 8
+#     else:
+#         print("Please pay $12")
+#         bill = 12
+#
+#     wants_photo = input("Do you want a photo taken? Y or N.")
+#     if wants_photo == "Y":
+#         bill = bill + 3
+#     print(f"Your final bill is ${bill}")
+#
+# else:
+#     print(("you cannot ride. Sorry"))
+
+#TREASURE ISLAND GAME
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+step1 = input("Left or right?\n")
+if(step1.lower() == "right"):
+    print("Game Over")
+else:
+    step2 = input("Swim or wait?\n")
+    if(step2.lower() == "swim"):
+        print("Game Over.")
+    else:
+        step3 = input("Which door? Red, Blue or Yellow?\n")
+        if((step3.lower() == "red") or (step3.lower() == "blue")):
+            print("Game Over.")
+        else:
+            print("You WIN!")
+
+
 
